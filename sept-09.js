b69076fd1bbe93249b2aebee78ec211a5992e02f
Ferry33-2021 <88565458+Ferry33-2021@users.noreply.github.com>
2024-10-14 21:48:50 -0500

    Add files via upload

---
+++ sept-09.js
@@ -0,0 +1,38 @@
+console.log("Hola Mundo");
+
+let numero1 = 45;
+let numero2 = 33;
+const pi = 3.1416;
+let estado = true;
+let direccion = "calle 3 # 8-26";
+let suma = numero1 + numero2;
+
+console.log(numero1);
+console.log(numero2);
+console.log(pi);
+console.log(estado);
+console.log(direccion);
+
+//concatenar valores 
+
+console.log('El primer valor es: '+ numero1);
+console.log('El segundo valor es: '+ numero2);
+
+
+//concatenar valore con el signo +
+console.log('La suma de '+ numero1 + ' y ' + numero2 + ' es: '+ suma);
+
+//concatenar valores con la coma
+console.log('La suma de' , numero1 , ' y ' , numero2 , 'es: ', suma);
+
+//concatenar valores con literal template
+console.log(`la suma de ${numero1} Y ${numero2} ES: ${suma}`);
+
+
+
+
+
+
+
+
+
